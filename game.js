27e16ccc92e1847b9b6c2897e4e0144d86b420f2
Zachary Droge <zdroge@Fullstack-Computer.local>
2022-05-02 10:25:37 -0500

    Fix: setting main to starter code

--- game.js
+++ game.js
@@ -1,41 +1,22 @@
-  // IIFE, phonetically Iffy
+let numTimesClicked = 0;
 
-    let numTimesClicked = 0;
+  function win() {
+  alert('You win!');
+  reset();
+}
 
-     function win() {
-      alert('You win!');
-      reset();
-    }
-
-    export function consoleThatLog(){
-      console.log("hey i was logged")
-    }
-    
-    export function reset() {
-      numTimesClicked = 0;
-    }
+function reset() {
+  numTimesClicked = 0;
+}
     
 
-  //)()
-
-
-  // we can default export one thing and one thing only
-export default function() {
+function click() {
   numTimesClicked++;
   console.log("cat");
   if (numTimesClicked === 10) win();
 }
 
 
-  // click() //  is equal to ---> 
-    //function() {
-    //   numTimesClicked++;
-    //   console.log("You've been clicked.");
-    //   if (numTimesClicked === 10) win();
-    // }
-
- 
-  
   
 /*
   1) look at simple App
@@ -43,11 +24,5 @@ export default function() {
   3) Make it secure
   4) Make it like node modules, to "share" code accross js files
   
-  webpack = module bundler
-  1) we need to install webpack and webpack-cli [ x ]
-  2) add script to package.json to build your files into a bundle
-  3) configure webpack
-
-
 */
 
--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const button = document.getElementById('button');
-// const click = require("./game.js") this is no good in the front end
- import click, {consoleThatLog as c, reset} from './game'
-//  import {consoleThatLog as c, reset} from './game'
 
- c();
 button.addEventListener('click', function() {
-  clickTheButtonFunc();
+  click();
 });
 
 /*  
